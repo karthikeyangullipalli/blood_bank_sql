793d1b99fdd6bb7c97066cf1509f26142c7d9f43
karthikeyangullipalli <karthikeyangullipalli@gmail.com>
2026-04-09 22:11:08 +0530

    Secured config, added env, fixed gitignore

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,10 @@
-const mysql= require("mysql2");
-const db= mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"Karthikeyan@4221",
-  database:"blood_donation"
+require('dotenv').config();
+const mysql = require("mysql2");
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-module.exports=db;
+
+module.exports = db;
